9b539bfc43975063b0515f00d817e31e2314b445
Thomaz Machado <thomaz.machado@xferatech.com>
2024-10-05 21:46:52 -0300

    add estrutura do projeto

---
+++ ExpenseTrack/ExpenseTrack.WebAPI/Program.cs
@@ -0,0 +1,32 @@
+using ExpenseTrack.Infra.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddInfrastructure(builder.Configuration); //Adicionando referencia ao DependencyInjection do Infra.Data
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
